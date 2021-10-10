a49e86ab4f9fae4f38fb9aa01aa9360d74991e1a
viniciusthr <viniciusdbdt@gmail.com>
2021-10-10 16:26:21 -0300

    Alterações

--- principal.py
+++ principal.py
@@ -10,7 +10,7 @@ class DadosCadastros:
 		
 	# Função para cadastrar um cliente.
 	def CadastrarCliente(self,Cliente):
-		Existe = BuscarCPF(Cliente.CPF)
+		Existe = self.BuscarCPF(Cliente.cpf)
 		if Existe == None:
 			self._ListaDeClientes.append(Cliente)
 			return True	
@@ -19,32 +19,33 @@ class DadosCadastros:
 		
 	# Função para adicionar uma conta.
 	def AdicionarConta(self,Conta):
-		Existe1 = BuscarCPF(Conta._titular.CPF)
-		Existe2 = BuscaConta(Conta.numero)
-		if Existe1 == None and Existe2 == None:
+		Existe1 = self.BuscarCPF(Conta._titular.cpf)
+		Existe2 = self.BuscarConta(Conta.numero)
+		Existe3 = self.BuscarPessoa(Conta._titular.cpf)
+		if Existe1 != None and Existe2 == None and Existe3 == None:
 			self._ListaDeContas.append(Conta)
 			return True
 		else:
 			return False
 	
 	# Função para procurar na lista se alguma pessoa tem o mesmo CPF.
-	def BuscaCPF(self,Procurado):
+	def BuscarCPF(self,Procurado):
 		for C in self._ListaDeClientes:
-			if C.CPF == Procurado:
+			if C.cpf == Procurado:
 				return C
 		return None
 	
 	# Função para procurar se o número da conta já está na lista de contas.
-	def BuscaConta(self,Procurado):
+	def BuscarConta(self,Procurado):
 		for C in self._ListaDeContas:
 			if C.numero == Procurado:
 				return C
 		return None
 	
 	# Função para verificar se aquele cliente possui uma conta na lista de contas.
-	def BuscaPessoa(self,Procurado):
+	def BuscarPessoa(self,Procurado):
 		for C in self._ListaDeContas:
-			if C.titular.CPF == Procurado:
+			if C.titular.cpf == Procurado:
 				return C
 		return None
 
